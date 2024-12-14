d090964b93ea3581395fb1969e1f5f4ace6f298a
Amardeep Rana <134470640+amardeeprana29@users.noreply.github.com>
2024-12-14 21:30:52 +0530

    Add files via upload

---
+++ Practice/-,+,0.js
@@ -0,0 +1,11 @@
+// Check if a number is Positive , negative, or zero.
+
+let num = -225;
+
+if(num>0){
+    console.log("The number",num,"is positive.");
+}else if(num<0){
+    console.log("The number",num,"is negative.");
+}else{
+    console.log("The number",num,"is zero.");
+}
