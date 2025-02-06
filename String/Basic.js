b3f18d14081c5e1bdca1e197f4a20f2bca9bd9ae
Amardeep Rana <134470640+amardeeprana29@users.noreply.github.com>
2025-02-06 23:36:06 +0530

    Add files via upload

---
+++ String/Basic.js
@@ -0,0 +1,26 @@
+// Print string by using arry
+
+let s = "Amardeep Rana";
+
+console.log(s[0]);
+console.log(s[1]);
+console.log(s[2]);
+console.log(s[3]);
+console.log(s[4]);
+console.log(s[5]);
+console.log(s[6]);
+console.log(s[7]);
+console.log(s[8]);
+console.log(s[9]);
+console.log(s[10]);
+console.log(s[11]);
+console.log(s[12]);
+
+console.log("*****************");
+console.log(s.length);
+
+// For using for loop
+
+for(let i=0; i<=s.length; i++){
+    console.log(s[i]);
+}
