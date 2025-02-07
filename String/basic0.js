3c189923a5c628614ac089415519328b4761c844
Amardeep Rana <134470640+amardeeprana29@users.noreply.github.com>
2025-02-07 23:06:13 +0530

    Add files via upload

---
+++ String/basic0.js
@@ -0,0 +1,11 @@
+// We can declare string using double quote and single quote
+
+let name = "Amardeep Rana";
+
+let names = 'Amardeep Rana';
+
+console.log(typeof(name));  // string
+
+console.log(typeof(names)); // string
+
+// typeof use to identify data type
