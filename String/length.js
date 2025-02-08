40fcbae4accf50df57631480a0970e1bde3e0b2f
Amardeep Rana <134470640+amardeeprana29@users.noreply.github.com>
2025-02-08 23:39:34 +0530

    Add files via upload

---
+++ String/length.js
@@ -0,0 +1,13 @@
+// Length concept
+
+let arr = [1,5,6,2,4,78];
+
+console.log("Length of an Array = ",arr.length);
+console.log("Last Index of an Array =",arr.length-1);
+
+// similarly
+
+let string = "Amardeep Rana";
+
+console.log("Lenth of String",string.length);
+console.log("Last index of string =",string.length-1);
